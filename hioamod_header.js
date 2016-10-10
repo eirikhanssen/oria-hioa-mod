7463b7d6679b4005fbc5e6a224f6bff0cb83250b
eirikhanssen <eirikhanssen79@gmail.com>
2016-10-10 10:52:26 +0200

    activate book tab on order page and hide the journals tab

---
+++ hioamod_header.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // bestillingssiden
+    if($('body').hasClass('EXLCitationLinker')) {
+        // aktiver bok-fane
+        $('#citationLinkerTopTab1').click();
+    
+        // skjul tidsskrift-fane
+        $('#citationLinkerTopTabContainer0').hide();
+    }
+});
